c705bb943aa3b8541dbd3b88753641a24cda207f
Raymour <raymour@hotmail.com>
2016-06-23 08:50:39 -0400

    Push1

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+// Question 1
+
+        int x = 7;
+
+        if (x % 2 == 0)
+            System.out.println("True");
+        else
+            System.out.println("False");
+
+//Question 2
+
+        String s = "something", t = "something1";
+
+            if (s.equals(t))
+                System.out.println("True");
+            else
+                System.out.println("False");
+//Question 3
+
+
+        int y;
+        for(y = 0; y<10; y = y+1)
+            System.out.println("This is y: " + y);
+
+
+        //Question 4
+
+     /*   Random r = new Random();
+
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                a[i][j] = r.nextInt();
+                */
+            }
+
+        }
+
+
+
+
+
+
+
+
